92a624e3e9f96e5c77d1366005b92b27e2ae9c5a
Andy Anderson <91595477+AndyAnderson8@users.noreply.github.com>
2022-12-05 21:28:08 -0800

    Add files via upload

--- day6/solution.py
+++ day6/solution.py
@@ -3,13 +3,7 @@ input = open('input.txt').read().splitlines()
 def packetBeginningIndex(markerLength):
   for line in input:
     for i in range(len(line) - markerLength):
-      marker = line[i:i + markerLength]
-      allUnique = True
-      for j, firstLetter in enumerate(marker):
-        for k, secondLetter in enumerate(marker):
-          if j > k and firstLetter == secondLetter:
-              allUnique = False
-      if allUnique:
+      if len(set(line[i:i + markerLength])) == markerLength:
         return i + markerLength
 
 print("Part 1:", packetBeginningIndex(4))
