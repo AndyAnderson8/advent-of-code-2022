9d020e25890df78770c09f5738d8237df5931f0b
Andy Anderson <91595477+AndyAnderson8@users.noreply.github.com>
2022-12-05 21:30:51 -0800

    Add files via upload

--- day6/solution.py
+++ day6/solution.py
@@ -1,10 +1,9 @@
-input = open('input.txt').read().splitlines()
+input = open('input.txt').read()
 
 def packetBeginningIndex(markerLength):
-  for line in input:
-    for i in range(len(line) - markerLength):
-      if len(set(line[i:i + markerLength])) == markerLength:
-        return i + markerLength
+  for i in range(len(input) - markerLength):
+    if len(set(input[i:i + markerLength])) == markerLength:
+      return i + markerLength
 
 print("Part 1:", packetBeginningIndex(4))
 print("Part 2:", packetBeginningIndex(14))
